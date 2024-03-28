ae87ea51002f6f75c55bb88e795096960d7e175b
Kirill <kirshuvl@gmail.com>
2024-03-29 01:58:47 +0400

    Update Header

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,30 +1,26 @@
-import { type Component, Show } from 'solid-js'
+import type { Component } from 'solid-js'
 
 import { A, useNavigate } from '@solidjs/router'
 
 import clsx from 'clsx'
 import { Button } from 'itpolygon-ui-dev'
+import { useSessionStateContext } from '../../context/session'
 import styles from './Header.module.scss'
 
 export const Header: Component = () => {
     const navigate = useNavigate()
+    const {
+        actions: { signOut },
+    } = useSessionStateContext()
+
     return (
         <header class={clsx(styles.header)}>
             <A href="/" class={clsx(styles.logo)}>
                 <img src="/public/icons/logo.png" alt="" /> ИТ Полигон
             </A>
             <div class={clsx(styles.buttons)}>
-                <Show
-                    when={true}
-                    fallback={
-                        <>
-                            <Button value="Войти" onClick={() => navigate('/login')} />
-                        </>
-                    }
-                >
-                    <Button value="Дашборд" onClick={() => navigate('/dashboard')} />
-                    <Button value="Выйти" outline />
-                </Show>
+                <Button value="Дашборд" onClick={() => navigate('/dashboard')} />
+                <Button value="Выйти" onClick={() => signOut()} outline />
             </div>
         </header>
     )
