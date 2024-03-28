df2f0be46b898243c4d60394d3896f88187c9cb8
Kirill <kirshuvl@gmail.com>
2024-03-29 02:14:37 +0400

    Update Layouts

--- src/routers/layouts/MainLayout/MainLayout.tsx
+++ src/routers/layouts/MainLayout/MainLayout.tsx
@@ -1,10 +1,20 @@
+import { useNavigate } from '@solidjs/router'
 import clsx from 'clsx'
-import type { ParentComponent } from 'solid-js'
+import { type ParentComponent, createEffect } from 'solid-js'
+import { useSessionStateContext } from '../../../context/session'
 import { Footer } from '../../../widgets/Footer'
 import { Header } from '../../../widgets/Header'
 import styles from './MainLayout.module.scss'
 
 export const MainLayout: ParentComponent = (props) => {
+    const { isAuthenticated } = useSessionStateContext()
+    const navigate = useNavigate()
+
+    createEffect(() => {
+        if (!isAuthenticated()) {
+            navigate('/')
+        }
+    })
     return (
         <div class="container">
             <div class={clsx(styles.content)}>
