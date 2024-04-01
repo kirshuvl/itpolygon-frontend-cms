8a76e950128131f0b616c58dac4f35a79d5ffb1d
Kirill <kirshuvl@gmail.com>
2024-04-01 04:42:52 +0400

    Update URL in GroupCard

--- src/components/GroupCard/GroupCard.tsx
+++ src/components/GroupCard/GroupCard.tsx
@@ -20,7 +20,7 @@ export const GroupCard: Component<Props> = (props) => {
     const group = props.group
     return (
         <>
-            <div onClick={() => navigate(`/cms/groups/${group.id}`)} class={clsx(styles.card)}>
+            <div onClick={() => navigate(`/groups/${group.id}`)} class={clsx(styles.card)}>
                 <div class={clsx(styles.icon)}>
                     <Show
                         when={group.icon}
